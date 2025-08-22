805f0ac695f99cc08d5192b7e0a566bc0d083ceb
Prakash Pander <prakashpander704@gmail.com>
2025-08-22 09:26:58 +0530

    some backend changes

--- Backend/utils/jwtToken.js
+++ Backend/utils/jwtToken.js
@@ -3,9 +3,13 @@ export const generateToken = (user, message, statusCode, res) => {
     const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
 
     res.status(statusCode).cookie(cookieName, token, {
+        
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
         httpOnly: true,
+         secure: process.env.NODE_ENV === "production" ? true : false, // ye line extra add h 
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // ye line extra add h 
+        
     }).json({
         success: true,
         message,
